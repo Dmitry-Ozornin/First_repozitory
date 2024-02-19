2da6b87a724b917ea35c5db87a330f649fb34d3d
Dmitry Ozornin <ozornindimitry@yandex.ru>
2024-02-19 20:19:52 +0300

    add some varilebles git:Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello world");
+﻿using System.Data;
+
+Console.WriteLine("Hello world");
+
+int V = 100;
+Console.WriteLine(V);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
