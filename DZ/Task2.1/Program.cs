e19c476563a8c1a19ff065be69420bbd7391e7fa
Dmitry Ozornin <ozornindimitry@yandex.ru>
2024-02-21 21:56:22 +0300

    решил первую задачу домаки второго семинара

---
+++ DZ/Task2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Enter number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ( num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine($"{num} кратное 7 и 23");
+}
+else 
+{
+    if ( num % 7== 0)
+    {
+       Console.WriteLine($"{num} кратное только 7"); 
+    }
+    else
+    {
+        if (num % 23 == 0)
+        {
+            Console.WriteLine($"{num} кратное только 23");
+        }
+        else
+        {
+            Console.WriteLine($"{num}  не кратное 7 или 23");
+        }
+    }
+}
