3eb72be4117224ec970d58622aedc1f79a8d65a5
Dmitry Ozornin <ozornindimitry@yandex.ru>
2024-02-21 23:59:06 +0300

    решил третью задачу

---
+++ DZ/Task2.3/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.WriteLine("ввчедите число из отрезка от 10 до 99: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (true)
+{
+    if (( n >= 10) && ( n <= 99)) break;
+    Console.WriteLine("число не из отрезка от 10 до 99: ");
+    Console.Write("ввчедите число из отрезка от 10 до 99: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+ int n1 = n % 10;
+ int n2 = n / 10;
+ if (n1 > n2)
+  {
+      Console.WriteLine($"Наибольшая цифра числа {n} {n1}");
+  }
+  else
+ {
+       Console.WriteLine($"Наибольшая цифра числа {n} {n2}");
+  }
+    
