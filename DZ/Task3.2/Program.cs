5ab905fe4406e2bb9ad0e59b383ae0c7cbddc918
Dmitry Ozornin <ozornindimitry@yandex.ru>
2024-02-28 02:56:43 +0300

    Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)

---
+++ DZ/Task3.2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+int[] Random_mas(int num)
+{
+    int[] mas = new int[num];
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] = new Random().Next(1, 10);
+       
+    }
+    return mas;
+    
+}
+void Print_mas(int[] mas)
+{
+    foreach (var i in mas)
+    {
+         Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+}
+
+
+int[] New_mas(int[] mas)
+{
+    
+        int[] new_mas = new int[mas.Length];
+        for (int i = 0; i < mas.Length; i++)
+        {
+            new_mas[i] = mas[mas.Length - 1 - i];
+        }
+        return new_mas;
+    
+}
+Console.Write("Введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arrey = Random_mas(n);
+Print_mas(arrey);
+Console.WriteLine(string.Join(" ",New_mas(arrey)));
