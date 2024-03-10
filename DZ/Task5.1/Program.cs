e2759038314038b400c854e062ef2f5d88d984bf
Dmitry Ozornin <ozornindimitry@yandex.ru>
2024-03-10 21:33:24 +0300

    Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.

---
+++ DZ/Task5.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+char[,] array = new char[,] {{'a', 'b', 'c'}, {'d', 'f', 'g'}};
+
+string st = string.Empty; // string.Empty или ""
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        st += array[i, j];
+    }
+}
+Console.WriteLine(st);
