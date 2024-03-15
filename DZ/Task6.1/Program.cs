9a8aa9e8a6bf2f2d574b335c290354b9822e25f4
Dmitry Ozornin <ozornindimitry@yandex.ru>
2024-03-15 23:04:44 +0300

    Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.

---
+++ DZ/Task6.1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+string GetListNum(int n, int m)
+{
+    if (m > n)
+    {
+        if (m == n)
+        {
+            return Convert.ToString(n);
+        }
+        return n + " " + GetListNum(n + 1, m);
+    }
+    if (m == n)
+    {
+        return Convert.ToString(m);
+    }
+    return m + " " + GetListNum(n, m + 1);
+}
+
+
+
+
+Console.WriteLine("введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{GetListNum(n, m)}");
