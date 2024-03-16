0e25f31d42253621ca47740c2732f3d431312f13
Dmitry Ozornin <ozornindimitry@yandex.ru>
2024-03-16 16:17:38 +0300

    Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.

---
+++ DZ/Task6.3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+Random random = new Random();
+
+int[] RandomArr(int size)
+{
+    int[] arr = new int[size];
+    Array(arr, size -1);
+    return arr;
+}
+
+void Array(int[] arr, int i)
+{
+    if (i < 0) return;
+    arr[i] = random.Next(0, 100);
+    Array(arr,i - 1);
+    
+    Console.Write($"{arr[i]} ");
+}
+
+void PrintArrayRevers(int[] arr, int i)
+{
+    if (i < 0) return;
+    Console.Write(arr[i] + " ");
+    PrintArrayRevers(arr, i -1);
+}
+
+Console.WriteLine(" Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = RandomArr(size);
+Console.WriteLine();
+Console.WriteLine(" массива в обратном порядке ");
+PrintArrayRevers(arr, size - 1);
